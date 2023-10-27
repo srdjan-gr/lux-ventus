eedaed138036464bfcb124e5db543248bffee461
srdjan-gr <srdjan.gr@gmail.com>
2023-10-27 22:05:38 +0200

    Color shades for main theme color add. Colors changed in html

---
+++ readme.md
@@ -0,0 +1,7 @@
+Lux-Ventus Landing page made from free Figma file.
+
+Layout made with Tailwindcss.
+
+Layout is responsive, with mobile menu.
+
+DEMO: https://lux-ventus.netlify.app/
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "main_green": "#4CE0D7",
         "light_green": "#defaf8",
         "main_black": "262626",
+
+        'theme_green': {
+          DEFAULT: '#4CE0D7',
+          50: '#E8FBFA',
+          100: '#D7F8F6',
+          200: '#B4F2EE',
+          300: '#92ECE7',
+          400: '#6FE6DF',
+          500: '#4CE0D7',
+          600: '#24D0C5',
+          700: '#1CA098',
+          800: '#13706B',
+          900: '#0B403D',
+          950: '#072926'
+        },
       },
       fontFamily: {
         'poppins_font': ['Poppins', 'sans-serif'],
