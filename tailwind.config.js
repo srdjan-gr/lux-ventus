a9f79fb2f74fc99ac2fdb8c37c7d4e6c9bae5359
srdjan-gr <srdjan.gr@gmail.com>
2023-10-27 13:56:15 +0200

    Initial commit. File setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main_green": "#4CE0D7",
+        "main_black": "262626",
+      }
+    },
+  },
+  plugins: [],
+}
+
