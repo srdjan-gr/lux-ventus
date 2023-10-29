4df4cb9689d7675c31f742b889b20bec9a7707ed
srdjan <srdjan.gr@gmail.com>
2023-10-29 17:59:04 +0100

    MObile menu for medium screens. Overlay for mobile menu for tablets

--- build/js/script.js
+++ build/js/script.js
@@ -3,13 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeBtn = document.getElementById('close-mobile-btn')
     const hamburger = document.getElementById('hamburger')
     const menu = document.getElementById('mobile-menu')
+    const overlay = document.getElementById('overlay')
 
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('active_mobile-menu')
+        overlay.classList.toggle('active_mobile-menu')
     })
 
     closeBtn.addEventListener('click', () => {
         menu.classList.toggle('active_mobile-menu')
+        overlay.classList.toggle('active_mobile-menu')
     })
 
 
