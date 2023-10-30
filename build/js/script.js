37e82bf4e4951b777cac3c0dc12207ddd72e8518
srdjan <srdjan.gr@gmail.com>
2023-10-30 17:17:51 +0100

    Search input add

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('mobile-menu')
     const overlay = document.getElementById('overlay')
 
+    const closeSearch = document.getElementById('close-search-btn')
+    const searchBtn = document.getElementById('search-btn')
+    const searchInput = document.getElementById('search-input')
+
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('active_mobile-menu')
         overlay.classList.toggle('active_mobile-menu')
@@ -15,5 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
         overlay.classList.toggle('active_mobile-menu')
     })
 
+
+
+
+    searchBtn.addEventListener('click', () => {
+        searchInput.classList.toggle('active_search')
+    })
+
+    closeSearch.addEventListener('click', () => {
+        searchInput.classList.toggle('active_search')
+    })
 
 })
